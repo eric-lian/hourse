e4c0253d06893efd8331cb4f1d5606e93509fd7c
连记伟 <lianjiwei@yixia.com>
2022-11-19 00:47:59 +0800

    feat : 找服务优化立即订阅按钮 ； 添加电话咨询

--- pages/look_service/look_service.js
+++ pages/look_service/look_service.js
@@ -36,13 +36,13 @@ Page({
     const defaultLookServiceName = getApp().globalData.defaultLookServiceName
     console.log("===============" + defaultLookServiceName)
     // 查找与当前选中的是否相同
-    let selectIndex  = this.data.currentSelectIndex
+    let selectIndex = this.data.currentSelectIndex
     console.log(selectIndex)
     this.data.services.find((value, index) => {
-        if(value._id == defaultLookServiceName){
-          selectIndex = index
-          return true
-        } 
+      if (value._id == defaultLookServiceName) {
+        selectIndex = index
+        return true
+      }
     })
     console.log(selectIndex)
     console.log(defaultLookServiceName)
@@ -138,11 +138,37 @@ Page({
   },
 
   immediately_subscribe(e) {
+    console.log(e)
     const merchant_id = e.currentTarget.dataset.merchant_id
     const merchant_name = e.currentTarget.dataset.merchant_name
+    const service_person_name = e.currentTarget.dataset.service_person_name
+    const service_person_id = e.currentTarget.dataset._id
+    const service_name = e.currentTarget.dataset.service_name
+    const merchant_phone = e.currentTarget.dataset.phone
+    const merchant_open_id = e.currentTarget.dataset.merchant_open_id
     console.log(merchant_id)
     wx.navigateTo({
-      url: '/pages/merchant_subscribe/merchant_subscribe?service_merchant_name=' + merchant_name + "&merchant_id=" + merchant_id
+      url: '/pages/merchant_subscribe/merchant_subscribe?service_merchant_name=' + merchant_name +
+      "&merchant_id=" + merchant_id +
+      "&merchant_open_id=" + merchant_open_id +
+      "&merchant_phone=" + merchant_phone +
+      "&service_person_name=" + service_person_name +
+      "&service_person_id=" + service_person_id +
+      "&service_type=" + service_name
+    })
+  },
+  gotoMerchantDetail(e) {
+    console.log(e)
+    const _id = e.currentTarget.dataset.merchant_id
+    wx.navigateTo({
+      url: '/pages/merchant_detail/merchant_detail?_id=' + _id,
+    })
+  },
+
+  immediately_call(e) {
+    const phone = e.currentTarget.dataset.phone
+    wx.makePhoneCall({
+      phoneNumber: phone,
     })
   }
 })
